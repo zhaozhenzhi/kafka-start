47a32f50c0835b50037285c768b2e68f8b1c6bf2
赵振智 <zhaozhenzhi@xinyunlian.com>
2019-03-25 17:13:17 +0800

    111

---
+++ consumerstart/src/main/java/cn/dazhiyy/kafka/Main.java
@@ -0,0 +1,41 @@
+package cn.dazhiyy.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+/**
+ * @author dazhi
+ * @projectName kafka-start-res
+ * @packageName cn.dazhiyy.kafka
+ * @className Main
+ * @description TODO
+ * @date 2019/3/25 16:37
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Properties prop = new Properties();
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("group.id", "test");
+        props.put("enable.auto.commit", "true");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("session.timeout.ms", "30000");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
+        consumer.subscribe(Arrays.asList("test"));
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(100);
+            for (ConsumerRecord<String, String> record : records){
+                System.out.printf("offset = %d, key = %s, value = %s\n", record.offset(), record.key(), record.value());
+            }
+        }
+
+
+    }
+}
