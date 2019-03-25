f11e8ee13f470e28d76a39dfcbb1c72b8eaeb03b
赵振智 <zhaozhenzhi@xinyunlian.com>
2019-03-25 16:30:51 +0800

    kafka 学习

---
+++ produce/src/main/java/cn/dazhiyy/kafka/Main.java
@@ -0,0 +1,44 @@
+package cn.dazhiyy.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+/**
+ * @author dazhi
+ * @projectName kafkaproducer
+ * @packageName cn.dazhiyy.kafka
+ * @className Main
+ * @description TODO
+ * @date 2019/3/25 15:30
+ */
+public class Main {
+
+
+    public static void main(String[] args) throws InterruptedException {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("acks", "all");
+        props.put("retries", 0);
+        props.put("batch.size", 16384);
+        props.put("linger.ms", 1);
+        props.put("buffer.memory", 33554432);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+
+        Producer<String, String> producer = new KafkaProducer<String, String>(props);
+        for(int i = 0; i < 100000; i++){
+            Thread.sleep(100L);
+            producer.send(new ProducerRecord<String, String>("test", Integer.toString(i), Integer.toString(i)));
+        }
+
+
+        producer.close();
+
+
+
+    }
+}
